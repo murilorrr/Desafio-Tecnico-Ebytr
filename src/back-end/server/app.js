07c18990d9cdce34953ac63b9583ebd4e733499e
Murilo Ribeiro <murilorsv14@gmail.com>
2022-02-11 08:51:31 -0300

    feat: init user route

--- src/back-end/server/app.js
+++ src/back-end/server/app.js
@@ -2,18 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { errorMiddleware } = require('../middleware');
 
-// const routers = require('../routes');
+const { user } = require('../routes');
 
 const app = express();
-
 app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({ extended: true }));
-// Não remover esse end-point, ele é necessário para o avaliador
-app.get('/', (request, response) => {
-  response.send();
-});
-// Não remover esse end-point, ele é necessário para o avaliador
+app.use('/user', user);
 
 app.use(errorMiddleware);
 
