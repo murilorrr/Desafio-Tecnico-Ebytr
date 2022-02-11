a33154242a3610d2037567c200f9ab258190504c
Murilo Ribeiro <murilorsv14@gmail.com>
2022-02-11 13:39:39 -0300

    remove deafult endpoint

--- src/back-end/server/app.js
+++ src/back-end/server/app.js
@@ -8,13 +8,6 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({ extended: true }));
-// Não remover esse end-point, ele é necessário para o avaliador
-app.get('/', (request, response) => {
-  response.send();
-});
-// Não remover esse end-point, ele é necessário para o avaliador
-
 app.use(errorMiddleware);
 
 module.exports = app;
