3e9974a8f3ce4d80a87b9d4fed32b518683b5e55
Murilo Ribeiro <murilorsv14@gmail.com>
2022-02-11 08:53:07 -0300

    feat: create full cycle to create a user and a fist connection with database

---
+++ src/back-end/service/user/createUser.js
@@ -0,0 +1,31 @@
+const Joi = require('joi');
+const { StatusCodes } = require('http-status-codes');
+const { errorHandler } = require('../../utils/index');
+const User = require('../../model')('User');
+
+const userSchema = Joi.object({
+  name: Joi.string().min(8),
+  email: Joi.string().email().required(),
+  password: Joi.string().length(6).required(),
+});
+
+const alreadyExists = async (email) => {
+  const user = await User.getOneByEmail(email);
+  console.log(user);
+  return user || null;
+};
+
+module.exports = async (user) => {
+  const { email } = user;
+  const { error } = userSchema.validate(user); 
+  if (error) throw errorHandler(StatusCodes.BAD_REQUEST, error.message);
+
+  const exists = await alreadyExists(email);
+  if (exists) throw errorHandler(StatusCodes.CONFLICT, 'User already registered');
+
+  // const token = generateJwt(user); // ToDo
+
+  await User.createOne(user);
+
+  return { user };
+};
