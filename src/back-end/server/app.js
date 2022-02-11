539f9bc4fc3b427b93cddd35b9d077e5e19000a2
Murilo Ribeiro <murilorsv14@gmail.com>
2022-02-11 13:51:24 -0300

    fix: add route without merged in merge from branch CreateUser

--- src/back-end/server/app.js
+++ src/back-end/server/app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { errorMiddleware } = require('../middleware');
 
-// const routers = require('../routes');
+const { user } = require('../routes');
 
 const app = express();
-
 app.use(bodyParser.json());
+
+app.use('/user', user);
 
 app.use(errorMiddleware);
 
