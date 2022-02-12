9c6164bf2231c1cd9b7d05a6608e70fafdf950fe
Murilo Ribeiro <murilorsv14@gmail.com>
2022-02-12 17:42:43 -0300

    feat: add login and create user pages

---
+++ src/front-end/src/pages/createUser.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+
+const createUser = () => {
+  return (
+    <div>Pagina de criar Usuário</div>
+  )
+}
+
+// createUser.propTypes = {}
+
+export default createUser
---
+++ src/front-end/src/pages/login.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const login = () => {
+  return (
+    <div>página de login</div>
+  )
+}
