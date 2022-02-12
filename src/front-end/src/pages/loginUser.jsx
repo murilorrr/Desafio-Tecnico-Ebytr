3e8cc349baace1755481240b701313ff8e8e675c
Murilo Ribeiro <murilorsv14@gmail.com>
2022-02-12 20:06:32 -0300

    feat: development the fist page

--- src/front-end/src/pages/loginUser.jsx
+++ src/front-end/src/pages/loginUser.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const loginUser = () => {
   return (
-    <div>página de login</div>
-  )
+    <div className='wrapper'>
+      <div className='login-img' />
+      <div className='login-form-div'>
+        <div className='login-form-and-greetings'>
+          <p className='greetings'>Welcome back</p>
+          <h2 className='login-header'>Login to your account</h2>
+          <div className='login-form'>
+            <form >
+              <div class="form-group">
+                <label for="InputEmail">Email</label>
+                <input type="email" class="form-control" id="InputEmail" placeholder="Enter email" />
+              </div>
+              <div class="form-group">
+                <label for="InputPassword">Password</label>
+                <input type="password" class="form-control" id="InputPassword" placeholder="Enter Password" />
+              </div>
+            </form>
+          </div>
+          <button type="submit" class="btn submit-button">Login now</button>
+        </div>
+        <p id='create-link'>Don't have an account yet? <Link to={'tasks'}>Join today</Link></p>
+      </div>
+    </div>
+  );
 }
 
 export default loginUser;
