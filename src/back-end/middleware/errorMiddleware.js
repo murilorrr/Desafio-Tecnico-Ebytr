becb7876258cb648076453a6a7141442c3c01a46
Murilo Ribeiro <murilorsv14@gmail.com>
2022-02-12 03:58:57 -0300

    fix(eslint): default value to error middleware to fix lint

--- src/back-end/middleware/errorMiddleware.js
+++ src/back-end/middleware/errorMiddleware.js
@@ -1,11 +1,9 @@
-const errorMiddleware = (err, req, res, _next) => {
-  console.log('-----------------------------', err);
-
+const errorMiddleware = (err, req, res, next) => {
   if (err.code) {
     return res.status(err.code).json(err);
   }
 
-  return res.status(500).json({ message: 'Internal Server Error' });
+  return res.status(500).json({ message: 'Internal Server Error' }) || next(err);
 };
 
 module.exports = errorMiddleware;
