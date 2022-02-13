cbf4975581c6feb1d265860c9f015377e365c77e
Murilo Ribeiro <murilorsv14@gmail.com>
2022-02-13 15:48:40 -0300

    setup to test endpoint in the app

---
+++ src/front-end/src/test/Endpoints.test.js
@@ -0,0 +1,33 @@
+import {
+  createUser,
+  loginUser,
+  getAllTasks,
+  createTask,
+  updateTask,
+} from '../api/api'
+
+import '@testing-library/jest-dom';
+
+
+const user = {
+	"name": "MuriloRibeiro",
+	"email": "murilorsv14@gmail.com",
+	"password": "aaaaaa"
+}
+
+describe('Teste de verificação dos endpoints usados para logar', () => {
+  
+  test('testa o endpoint createUser', async () => {
+    const response = await createUser(user)
+    expect(response.body)
+   })
+  
+  test('testa o endpoint loginUser', () => {  })
+  
+  test('testa o endpoint getAllTasks', () => {  })
+  
+  test('testa o endpoint createTask', () => {  })
+  
+  test('testa o endpoint updateTask', () => {  })
+  
+});
