86037a997e76b26223fa2f3d81d81442074ba1e7
Murilo Ribeiro <murilorsv14@gmail.com>
2022-02-13 15:46:56 -0300

    test(Routes): test 3 full routes the react-app

---
+++ src/front-end/src/test/Rotas.test.js
@@ -0,0 +1,52 @@
+import React from 'react';
+// import  from '../pages';
+
+import { render, screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+import App from '../App';
+
+import '@testing-library/jest-dom';
+
+describe('1 - Teste de Rotas', () => {
+  let history;
+
+  beforeEach(() => {
+    history = createMemoryHistory();
+  });
+
+  test('Rota /', () => {
+    render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+    history.push('/');
+    expect(screen.getByText(/Welcome back/i)).toBeInTheDocument();
+  });
+
+  test('Rota /createUser', () => {
+    render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+    history.push('/createUser');
+    expect(screen.getByText(/Welcome/i)).toBeInTheDocument();
+  });
+  
+  test('Rota /tasks', () => {
+    render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+    history.push('/');
+    expect(screen.getByText(/Hello/i)).toBeInTheDocument();
+  });
+  // test('Rota /, quando a rota for "/"', () => {
+  //   history.push('/');
+  //   expect(screen.getByText(/in Development/i)).toBeInTheDocument();
+  // });
+});
