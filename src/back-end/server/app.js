b073b0caca3047eb971b2e9e563ecf091fc6415c
Murilo Ribeiro <murilorsv14@gmail.com>
2022-02-14 11:00:34 -0300

    feat&build:add express rate limit in module

--- src/back-end/server/app.js
+++ src/back-end/server/app.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 const { errorMiddleware } = require('../middleware');
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+// src=https://www.npmjs.com/package/express-rate-limit
 
 const { user, login, token } = require('../routes');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+app.use(limiter);
 
 app.use('/user', user);
 
