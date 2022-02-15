71dbd1a3fa26de600db074332591acfbaa115d7f
Murilo Ribeiro <murilorsv14@gmail.com>
2022-02-15 15:49:17 -0300

    feat: getAllTask return if the task is yours or not

--- src/back-end/controller/task/getAllTask.js
+++ src/back-end/controller/task/getAllTask.js
@@ -1,9 +1,10 @@
 const { StatusCodes } = require('http-status-codes');
 const taskService = require('../../service/task');
 
-module.exports = async (_req, res, next) => {
+module.exports = async (req, res, next) => {
+  const userId = req.headers.id;
   try {
-    const result = await taskService.getAll();
+    const result = await taskService.getAll(userId);
     return res.status(StatusCodes.OK).json(result);
   } catch (error) {
     return next(error);
