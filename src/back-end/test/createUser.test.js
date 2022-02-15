871c364bd4a0ad3a98770ed5a5913942c8ccecd5
Murilo Ribeiro <murilorsv14@gmail.com>
2022-02-15 01:14:16 -0300

    refactor: to use connection mock injection and delete after each operation

--- src/back-end/test/createUser.test.js
+++ src/back-end/test/createUser.test.js
@@ -1,6 +1,7 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const sinon = require('sinon');
+const connectionMock = require('./connectionMock');
 
 const { expect } = chai;
 
@@ -11,7 +12,6 @@ chai.use(chaiHttp);
 // mock de banco de dados em memoria
 
 const { MongoClient } = require('mongodb');
-const { MongoMemoryServer } = require('mongodb-memory-server');
 
 //
 
@@ -25,49 +25,43 @@ const user = {
 
 describe('POST /user', () => {
   describe('É esperado ao se cadastrar um usuário:', () => {
-    let DBServer;
     let response;
+    let connection;
+    let db;
 
-    afterEach(async () => {
+    before( async () => {
+      connection = await connectionMock();
+
+        sinon.stub(MongoClient, 'connect')
+          .resolves(connection);
+        
+        db = connection.db('ToDo-Ebytr');
+    });
+
+    after( async () => {
       MongoClient.connect.restore();
-      await DBServer.stop();
     });
 
     describe('quando é criado com sucesso', () => {
       beforeEach(async () => {
-        DBServer = new MongoMemoryServer();
-        const URLMock = await DBServer.getUri();
-        const connectionMock = MongoClient.connect(
-          URLMock,
-          { useNewUrlParser: true, useUnifiedTopology: true },
-        );
-
-        sinon.stub(MongoClient, 'connect')
-          .resolves(connectionMock);
-
         response = await chai.request(server)
           .post('/user')
           .set('content-type', 'application/json')
           .send(user);
       });
 
-      it('retorna o código de status 201 CREATED', () => {
+      afterEach(async () => {
+        await db.collection('User').deleteMany({});
+      });
+
+      it('retorna o código de status 201 CREATED com um objeto User', () => {
         expect(response).to.have.status(201);
-      });
 
-      it('retorna um objeto', () => {
         expect(response.body).to.be.a('object');
-      });
 
-      it('o objeto possui a propriedade "user"', () => {
         expect(response.body).to.have.property('user');
-      });
-
-      it('a propriedade "user" possui o objeto de usuário', () => {
-        const expectedUser = user;
-
-        expect(response.body.user.email).to.be.equal(expectedUser.email);
-        expect(response.body.user.name).to.be.equal(expectedUser.name);
+        expect(response.body.user.email).to.be.equal('rogerinho@gmail.com');
+        expect(response.body.user.name).to.be.equal('Rogerinho');
       });
 
       it('tem a propriedade _id do mongoDB', () => {
@@ -76,16 +70,9 @@ describe('POST /user', () => {
     });
 
     describe('quando é criado com falha', () => {
-      beforeEach(async () => {
-        DBServer = new MongoMemoryServer();
-        const URLMock = await DBServer.getUri();
-        const connectionMock = MongoClient.connect(
-          URLMock,
-          { useNewUrlParser: true, useUnifiedTopology: true },
-        );
 
-        sinon.stub(MongoClient, 'connect')
-          .resolves(connectionMock);
+      afterEach(async () => {
+        await db.collection('User').deleteMany({});
       });
 
       it('É retornado status 400 Bad Request', async () => {
@@ -125,8 +112,6 @@ describe('POST /user', () => {
           })
           .end((err, res) => {
             if (err) done(err);
-            expect(res.status).to.be.equal(400);
-            expect(res.body).be.a('object');
             expect(res.body).to.be.deep.equal({ code: 400, message: '"email" is required' });
             done();
           });
@@ -142,8 +127,6 @@ describe('POST /user', () => {
           })
           .end((err, res) => {
             if (err) done(err);
-            expect(res.status).to.be.equal(400);
-            expect(res.body).be.a('object');
             expect(res.body).to.be.deep.equal({ code: 400, message: '"password" is required' });
             done();
           });
