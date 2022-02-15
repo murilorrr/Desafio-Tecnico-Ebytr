7982648c409797c9984bb08d7c684050a380cc4a
Murilo Ribeiro <murilorsv14@gmail.com>
2022-02-15 16:41:52 -0300

    fix: add more limit in request to test

--- src/back-end/server/app.js
+++ src/back-end/server/app.js
@@ -6,7 +6,7 @@ const { errorMiddleware } = require('../middleware');
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  max: 120, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
