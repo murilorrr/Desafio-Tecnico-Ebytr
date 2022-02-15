158c2e494a5ce04471f5aafdf5dafb0a6b858b90
Murilo Ribeiro <murilorsv14@gmail.com>
2022-02-15 19:23:29 -0300

    feat: is not possible create a task with duplicate title and body

--- src/back-end/service/task/createTask.js
+++ src/back-end/service/task/createTask.js
@@ -14,6 +14,11 @@ module.exports = async (task) => {
   const { error } = taskSchema.validate(task);
   if (error) throw errorHandler(StatusCodes.BAD_REQUEST, error.message);
 
+  // se houver uma task com esse titulo e esse body throw error
+  const alreadExist = await Task.getOneTask(task.title, task.body);
+
+  if (alreadExist) throw errorHandler(StatusCodes.CONFLICT, 'Alread Exists');
+
   await Task.createOne(task);
 
   return { task };
