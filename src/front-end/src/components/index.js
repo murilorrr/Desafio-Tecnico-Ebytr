3c6f097eacb0f8f11e77770d2675a3735ed33594
Murilo Ribeiro <murilorsv14@gmail.com>
2022-02-16 00:45:02 -0300

    add components to export default index

--- src/front-end/src/components/index.js
+++ src/front-end/src/components/index.js
@@ -1,1 +1,4 @@
-export { default as CreateTask } from './CreateTask'
+export { default as CreateTask } from './createTask'
+export { default as TaskList } from './listTasks'
+export { default as TaskCard } from './taskCard'
+export { default as Warning } from './warning'
