c7206a832252af7b98afae9d48df81fbd7db013d
Murilo Ribeiro <murilorsv14@gmail.com>
2022-02-16 01:52:40 -0300

    refactor: componentizer warning

--- src/front-end/src/pages/tasks.jsx
+++ src/front-end/src/pages/tasks.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { useHistory } from 'react-router-dom';
 import '../assets/css/tasksPage.css'
-import { CreateTask } from '../components'
+import { CreateTask, TaskList } from '../components'
 
 const Tasks = () => {
+
+  const history = useHistory();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      localStorage.clear();
+      history.push('/')
+    }
+  }, [history])
+
   return (
     <div className='tasksPage'>
       <CreateTask />
       <div>FILTER TASK</div>
-      <div>ALL TAKS</div>
+      <TaskList></TaskList>
     </div>
   )
 };
