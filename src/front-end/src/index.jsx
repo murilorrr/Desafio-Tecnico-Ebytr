79150590da949b49968ca1fa0a2550b4c4cf3331
Murilo Ribeiro <murilorsv14@gmail.com>
2022-02-16 13:23:14 -0300

    docs: components will be start with uppercase

--- src/front-end/src/index.jsx
+++ src/front-end/src/index.jsx
@@ -1,8 +1,10 @@
+/* eslint-disable import/no-named-as-default */
+/* eslint-disable import/no-named-as-default-member */
 import React from 'react';
 import { Provider } from 'react-redux';
-import App from './App';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './app/store';
 
