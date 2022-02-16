5994e4881044ca127350e2589c727f5eabc76af1
Murilo Ribeiro <murilorsv14@gmail.com>
2022-02-16 01:13:52 -0300

    create redux action creators and reducer

---
+++ src/front-end/src/app/slices/task.js
@@ -0,0 +1,33 @@
+import { createSlice} from "@reduxjs/toolkit";
+
+export const taskSlice = createSlice({
+  name:'task', 
+  initialState: {
+    visualizationTasks: [],
+    allTasks: [],
+  },
+  reducers: {
+    createTaskAction: (state, action) => { state.allTasks.push(action.payload) },
+    getAllTaskAction: (state, action) => {
+      state.allTasks = action.payload;
+    },
+    // deleteTask: (state, action) => {
+    //   state.allTasks.splice(indexOf(action.payload), 1);
+    //   state.visualizationTasks.splice(indexOf(action.payload), 1);
+    // },
+    // updateTask: (state, action) => {
+    //   state.allTasks.push();
+    // },
+    filterTask: (state, action) => { state.visualizationTasks = action.payload },
+    initVisualization: (state) => {
+      state.visualizationTasks = state.allTasks
+    }
+  }
+})
+
+export const {
+  createTaskAction,
+  initVisualization,
+  getAllTaskAction, filterTask } = taskSlice.actions;
+
+export default taskSlice.reducer;
--- src/front-end/src/app/store.js
+++ src/front-end/src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import taskReducer from './slices/task'
 
 export const store = configureStore({
-  reducer: {}
+  reducer: {
+    task: taskReducer
+  }
 })
