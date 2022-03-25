a596a37336bdc9346c19d417bf066922b1fdbdba
Murilo Ribeiro <murilorsv14@gmail.com>
2022-03-25 10:40:22 -0300

    add coverage and factory user

--- src/front-end/src/test/Endpoints.test.jsx
+++ src/front-end/src/test/Endpoints.test.jsx
@@ -1,20 +1,37 @@
 import { createUser, loginUser, getAllTasks, createTask, updateTask } from '../api/api';
+import { faker } from '@faker-js/faker';
 
 import '@testing-library/jest-dom';
 
-const user = {
-  name: 'MuriloRibeiro',
-  email: 'novo3Email@gmail.com',
-  password: 'projetoToDo'
+const factoryUser = () => {
+  
+  let randomName = '';
+  const minNameLength = 13;
+  while (randomName.length < minNameLength) {
+    randomName = faker.name.findName();
+  }
+
+  const splitedName = randomName.split(' ');
+
+  const passwordLength = 12;
+  const randomEmail = faker.internet.email(splitedName[0], splitedName[1]);
+  const randomPassword = faker.random.alphaNumeric(passwordLength);
+  const userFake = { name: randomName, email: randomEmail, password: randomPassword };
+
+  return userFake;
 };
+
+const user = factoryUser();
 
 describe('Teste de verificação dos endpoints usados para logar', () => {
   test('testa o endpoint createUser', async () => {
     const { data: {user: {name, email, password, _id: id}} } = await createUser(user);
+
     expect(name).toBe(user.name);
     expect(email).toBe(user.email);
     expect(password).toBe(user.password);
     expect(id).not.toBeNull();
+
   });
 
   test('testa o endpoint loginUser', async () => {
